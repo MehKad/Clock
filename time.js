0b7656d40a39a0976782b9d8fe6f9d06cdec1a5d
MehKad <mehkadiri@gmail.com>
2023-01-13 21:43:12 +0000

    Adding abit of style

--- time.js
+++ time.js
@@ -6,12 +6,24 @@ let hou = document.querySelector(".hou");
 let min = document.querySelector(".min");
 let sec = document.querySelector(".sec");
 
-setInterval(() => {
-  h.innerHTML = new Date().getHours();
-  m.innerHTML = new Date().getMinutes();
-  s.innerHTML = new Date().getSeconds();
+let h_dots = document.querySelector(".h_dots");
+let m_dots = document.querySelector(".m_dots");
+let s_dots = document.querySelector(".s_dots");
 
-  hou.style.strokeDashoffset = 440 - (440 * new Date().getHours()) / 24;
-  min.style.strokeDashoffset = 440 - (440 * new Date().getMinutes()) / 60;
-  sec.style.strokeDashoffset = 440 - (440 * new Date().getSeconds()) / 60;
+setInterval(() => {
+  hh = new Date().getHours();
+  mm = new Date().getMinutes();
+  ss = new Date().getSeconds();
+
+  h.innerHTML = hh;
+  m.innerHTML = mm;
+  s.innerHTML = ss;
+
+  hou.style.strokeDashoffset = 440 - (440 * hh) / 24;
+  min.style.strokeDashoffset = 440 - (440 * mm) / 60;
+  sec.style.strokeDashoffset = 440 - (440 * ss) / 60;
+
+  h_dots.style.transform = `rotate(${hh * 15}deg)`;
+  m_dots.style.transform = `rotate(${mm * 6}deg)`;
+  s_dots.style.transform = `rotate(${ss * 6}deg)`;
 });
