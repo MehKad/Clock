f0a348b3055fe0ae688278e00a11cc15f3332aff
MehKad <mehkadiri@gmail.com>
2023-01-13 20:52:53 +0000

    Adding interval

--- time.js
+++ time.js
@@ -1,7 +1,8 @@
 let h = document.querySelector(".hours");
 let m = document.querySelector(".minutes");
 let s = document.querySelector(".seconds");
-
-h.innerHTML = new Date().getHours();
-m.innerHTML = new Date().getMinutes();
-s.innerHTML = new Date().getSeconds();
+setInterval(() => {
+   h.innerHTML = new Date().getHours();
+   m.innerHTML = new Date().getMinutes();
+   s.innerHTML = new Date().getSeconds();
+})
